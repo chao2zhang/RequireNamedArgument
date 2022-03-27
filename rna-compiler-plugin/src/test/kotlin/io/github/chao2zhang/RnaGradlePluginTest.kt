45c050561e731af9723643f23a4b163ac8a61f09
Eliezer Graber <eygraber@gmail.com>
2022-03-27 01:15:44 -0400

    Allow unnamed trailing lambdas

--- rna-compiler-plugin/src/test/kotlin/io/github/chao2zhang/RnaGradlePluginTest.kt
+++ rna-compiler-plugin/src/test/kotlin/io/github/chao2zhang/RnaGradlePluginTest.kt
@@ -45,6 +45,77 @@ class RnaGradlePluginTest {
         val result = compile(listOf(REQUIRE_NAMED_ARGUMENT, main))
         assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
     }
+
+  @Test
+  fun allowsUnnamedTrailingLambda() {
+    val main = SourceFile.kotlin("main.kt", """
+            import io.github.chao2zhang.RequireNamedArgument
+
+            @RequireNamedArgument
+            fun functionWithLambda(a: () -> Unit) = a()
+
+            @RequireNamedArgument
+            fun functionWith2Lambdas(a: () -> Unit, b: () -> Unit) = a()
+
+            @RequireNamedArgument
+            fun functionWithLambdaAndOtherParameter(a: Int, b: () -> Unit) = b()
+
+            fun call() {
+                functionWithLambda { 1 }
+                functionWithLambda(a = { 1 })
+                functionWith2Lambdas(a = { 1 }) { 2 }
+                functionWith2Lambdas(a = { 1 }, b = { 2 })
+                functionWithLambdaAndOtherParameter(a = 1) { 1 }
+                functionWithLambdaAndOtherParameter(a = 1, b = { 1 })
+            }
+        """.trimIndent())
+    val result = compile(listOf(REQUIRE_NAMED_ARGUMENT, main))
+    assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+  }
+
+  @Test
+  fun doesNotAllowUnnamedNonTrailingLambda() {
+    val main = SourceFile.kotlin("main.kt", """
+            import io.github.chao2zhang.RequireNamedArgument
+
+            @RequireNamedArgument
+            fun functionWithLambda(a: () -> Unit) = a()
+
+            @RequireNamedArgument
+            fun functionWith2Lambdas(a: () -> Unit, b: () -> Unit) = a()
+
+            @RequireNamedArgument
+            fun functionWithLambdaAndOtherParameter(a: Int, b: () -> Unit) = b()
+
+            fun call() {
+                functionWithLambda({ 1 })
+                functionWith2Lambdas({ 1 }, { 2 })
+                functionWith2Lambdas(a = { 1 }, { 2 })
+                functionWith2Lambdas({ 1 }, b = { 2 })
+                functionWithLambdaAndOtherParameter(a = 1, { 1 })
+            }
+        """.trimIndent())
+    val result = compile(listOf(REQUIRE_NAMED_ARGUMENT, main))
+    assertEquals(KotlinCompilation.ExitCode.COMPILATION_ERROR, result.exitCode)
+    assertTrue(result.messages.contains(
+      "(13, 24): Argument '{ 1 }' should have name 'a' because the target function is @RequireNamedArgument."
+    ))
+    assertTrue(result.messages.contains(
+      "(14, 26): Argument '{ 1 }' should have name 'a' because the target function is @RequireNamedArgument."
+    ))
+    assertTrue(result.messages.contains(
+      "(14, 33): Argument '{ 2 }' should have name 'b' because the target function is @RequireNamedArgument."
+    ))
+    assertTrue(result.messages.contains(
+      "(15, 37): Argument '{ 2 }' should have name 'b' because the target function is @RequireNamedArgument."
+    ))
+    assertTrue(result.messages.contains(
+      "(16, 26): Argument '{ 1 }' should have name 'a' because the target function is @RequireNamedArgument."
+    ))
+    assertTrue(result.messages.contains(
+      "(17, 48): Argument '{ 1 }' should have name 'b' because the target function is @RequireNamedArgument."
+    ))
+  }
 }
 
 private fun compile(
