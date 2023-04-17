771f280d1e0584ce2ad124bc38935c0a803daede
Chao Zhang <chao.zhang@instacart.com>
2023-04-16 17:03:23 -0700

    Test detekt compiler plugin

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,10 +1,23 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("io.github.chao2zhang.rna") version "0.2.0"
+    kotlin("jvm") version "1.8.20"
+    id("io.github.detekt.gradle.compiler-plugin") version "1.23.0-RC1"
 }
 
 repositories {
     jcenter()
+}
+
+detekt {
+    config = files("config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+    debug = true
+    enableCompilerPlugin.set(true)
+    reports {
+        xml.enabled = false
+        html.enabled = false
+        sarif.enabled = false
+        txt.enabled = false
+    }
 }
 
 dependencies {
